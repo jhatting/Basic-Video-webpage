93381466cb3442b8826fdf5dcd2aa62696a49469
jhatting <jarrodhatting@icloud.com>
2023-04-28 15:04:41 -0500

    video Galley header

---
+++ my-app/src/component/ReactVideoGallery.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ReactVideoGallery = () => {
+    return (
+        <>
+            <h1 style={{textAlign: 'center'}}>Video Gallery</h1>
+        </>
+    )
+};
+
+export default ReactVideoGallery;
